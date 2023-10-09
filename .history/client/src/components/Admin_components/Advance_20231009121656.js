663a5f0d9256abbc1ab056cbd82bc13541052a8f
qais haidari <qaishaidari555@gmail.com>
2023-10-09 12:41:12 +1030

    new push 12:41 9/10/23

---
+++ .history/client/src/components/Admin_components/Advance_20231009121656.js
@@ -0,0 +1,7 @@
+import React, { Fragment, useState, useEffect, useRef } from "react";
+import Layout from "../../App_1";
+import Nav from "./Nav";
+import axios from "axios";
+export default function Advance() {
+  return <div></div>;
+}
